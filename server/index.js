0bc3abce099643c2a45a050f37cd9bdad18f4c51
EremonGoddy <eremon.godwin@gmail.com>
2025-02-18 08:04:17 +0300

    update

--- server/index.js
+++ server/index.js
@@ -1,10 +1,34 @@
 const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2");
+const bodyParser = require("body-parser");
+const multer = require('multer');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
 const app = express();
 
-app.use("/", (req, res)=>{
-res.send("Server is running.");
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const db = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'Goddinare3458##',
+  database: 'ebursary',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-app.listen(5000, () => {
-console.log("server is running on PORT 5000");
+// Example API route
+app.get("/", (req, res) => {
+  res.send("Server is running");
 });
+
+// Export the app for serverless deployment
+module.exports = app;
